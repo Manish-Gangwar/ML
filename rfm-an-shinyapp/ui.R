abc212a32e5837da6cf6c8a8b88f866115888d43
mgang2 <49343610+mgang2@users.noreply.github.com>
2020-12-03 18:18:13 +0530

    k

--- rfm-an-shinyapp/ui.R
+++ rfm-an-shinyapp/ui.R
@@ -6,7 +6,18 @@
 # 
 #    http://shiny.rstudio.com/
 #
+if (!require(shiny)){install.packages("shiny")}
+if (!require(RColorBrewer)){install.packages("RColorBrewer")}
+if (!require(ggplot2)){install.packages("ggplot2")}
+if (!require(dplyr)){install.packages("dplyr")}
+if (!require(rfm)) {install.packages("rfm")}
+if (!require(lubridate)) {install.packages("lubridate")}
 
+library(lubridate)
+library(rfm)
+library(dplyr)
+library(RColorBrewer)
+library(ggplot2)
 library(shiny)
 
 # Define UI for application that draws a histogram
--- text-topic-analysis-shinyapp/ui.R
+++ text-topic-analysis-shinyapp/ui.R
@@ -2,6 +2,17 @@
 #               Topic  Analysis             #
 #################################################
 
+if(!require(shiny)) {install.packages("shiny")}
+if(!require(text2vec)) {install.packages("text2vec")}
+if(!require(tm)) {install.packages("tm")}
+if(!require(tokenizers)) {install.packages("tokenizers")}
+if(!require(wordcloud)) {install.packages("wordcloud")}
+if(!require(slam)) {install.packages("slam")}
+if(!require(maptpx)) {install.packages("maptpx")}
+if(!require(igraph)) {install.packages("igraph")}
+
+
+library(igraph)
 library(shiny)
 library(text2vec)
 library(tm)
