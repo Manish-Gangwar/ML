0a8cd779b89f423d2d8e14a1cb2c505536f6107a
mgang2 <49343610+mgang2@users.noreply.github.com>
2020-12-03 18:38:17 +0530

    k

---
+++ dependency-ML.R
@@ -0,0 +1,86 @@
+try(require(shiny) || install.packages("shiny"))
+try(require("devtools")||install.packages("devtools"))
+try(require("magick")||install.packages("magick"))
+try(require(text2vec) || install.packages("text2vec"))
+try(require(tm) || install.packages("tm"))
+try(require(tokenizers) || install.packages("tokenizers"))
+try(require(wordcloud) || install.packages("wordcloud"))
+try(require(slam) || install.packages("slam"))
+try(require(stringi) || install.packages("stringi"))
+try(require(magrittr) || install.packages("magrittr"))
+try(require(tidytext) || install.packages("tidytext"))
+try(require(dplyr) || install.packages("dplyr"))
+try(require(tidyr) || install.packages("tidyr"))
+try(require(igraph)|| install.packages("igraph"))
+try(require(visNetwork)|| install.packages('visNetwork'))
+try(require("SnowballC")||install.packages("SnowballC"))
+try(require("psych")||install.packages("psych"))
+try(require("DBI")||install.packages("DBI"))
+try(require("assertthat")||install.packages("assertthat"))
+try(require("Rcpp")||install.packages("Rcpp"))
+try(require("mnormt")||install.packages("mnormt"))
+try(require("tidyr")||install.packages("tidyr"))
+try(require("ggplot2")||install.packages("ggplot2"))
+try(require("DT")||install.packages("DT"))
+try(require("reshape2")||install.packages("reshape2"))
+try(require("wordcloud")||install.packages("wordcloud"))
+try(require("plotly")||install.packages("plotly"))
+try(require("tidytext")||install.packages("tidytext"))
+try(require("tesseract")||install.packages("tesseract"))
+try(require("magrittr")||install.packages("magrittr"))
+try(require("caret")||install.packages("caret"))
+try(require("rpart")||install.packages("rpart"))
+try(require("rpart.plot")||install.packages("rpart.plot"))
+try(require("randomForest")||install.packages("randomForest"))
+try(require("dplyr")||install.packages("dplyr"))
+try(require("Hmisc")||install.packages("Hmisc"))
+try(require("randomForest")||install.packages("randomForest"))
+try(require("hydroGOF")||install.packages("hydroGOF"))
+
+suppressPackageStartupMessages({
+  try(require("pastecs")||install.packages("pastecs"))
+  try(require("rpart")||install.packages("rpart"))
+  try(require("dplyr")||install.packages("dplyr"))
+  try(require("Hmisc")||install.packages("Hmisc"))
+  try(require("randomForest")||install.packages("randomForest"))
+  try(require("hydroGOF")||install.packages("hydroGOF"))
+  
+  try(require("party")||install.packages("party"))
+  try(require("partykit")||install.packages("partykit"))
+})
+
+if(!require(text2vec)) {install.packages("text2vec")}
+if(!require(tm)) {install.packages("tm")}
+if(!require(tokenizers)) {install.packages("tokenizers")}
+if(!require(wordcloud)) {install.packages("wordcloud")}
+if(!require(slam)) {install.packages("slam")}
+if(!require(maptpx)) {install.packages("maptpx")}
+if(!require(igraph)) {install.packages("igraph")}
+if (!require("nFactors")) {install.packages("nFactors")}
+if (!require("qgraph")) {install.packages("qgraph")}
+if (!require("corrplot")) {install.packages("corrplot")}
+if(!require("pastecs")){install.packages("pastecs")}
+if(!require("RColorBrewer")){install.packages("RColorBrewer")}
+if(!require("Hmisc")){install.packages("Hmisc")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("reshape2")){install.packages("reshape2")}
+if (!require(RColorBrewer)){install.packages("RColorBrewer")}
+if (!require(ggplot2)){install.packages("ggplot2")}
+if (!require(dplyr)){install.packages("dplyr")}
+if (!require(rfm)) {install.packages("rfm")}
+if (!require(lubridate)) {install.packages("lubridate")}
+if (!require("ggbiplot")) {install_github("ggbiplot","vqv")}
+if (!require("plyr")) {install.packages("plyr")}
+if (!require("scales")) {install.packages("scales")}
+if (!require("grid")) {install.packages("grid")}
+if (!require("cluster")) {install.packages("cluster")}
+if (!require("mclust")) {install.packages("mclust")}
+if (!require("MASS")) {install.packages("MASS")}
+if (!require("scales")) {install.packages("scales")}
+if (!require("gridExtra")) {install.packages("gridExtra")}
+
+
+
+
+
+
