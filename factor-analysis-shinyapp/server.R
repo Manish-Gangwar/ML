eb1abc3ce829523bff6032cf111782ce5e49ad3f
mgang2 <49343610+mgang2@users.noreply.github.com>
2020-12-03 17:33:21 +0530

    k

--- factor-analysis-shinyapp/server.R
+++ factor-analysis-shinyapp/server.R
@@ -1,6 +1,10 @@
 #################################################
 #               Factor Analysis                 #
 #################################################
+if (!require("shiny")) {install.packages("shiny")}
+if (!require("nFactors")) {install.packages("nFactors")}
+if (!require("qgraph")) {install.packages("qgraph")}
+if (!require("corrplot")) {install.packages("corrplot")}
 
 library("shiny")
 library("nFactors")
