f17f3535490ae4336eebe65177f9eba4870f47a2
mgang2 <49343610+mgang2@users.noreply.github.com>
2020-12-26 20:18:29 +0530

    k

---
+++ Basic Statistics/server.R
@@ -0,0 +1,197 @@
+#################################################
+#      Summary & Basic Stats                      #
+#################################################
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("pastecs")){install.packages("pastecs")}
+if(!require("RColorBrewer")){install.packages("RColorBrewer")}
+if(!require("Hmisc")){install.packages("Hmisc")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("reshape2")){install.packages("reshape2")}
+if (!require("corrplot")) {install.packages("corrplot")}
+if (!require("PerformanceAnalytics")) {install.packages("PerformanceAnalytics")}
+if (!require("EnvStats")) {install.packages("EnvStats")}
+
+library(shiny)
+library(pastecs)
+library(RColorBrewer)
+library(Hmisc)
+library(ggplot2)
+library(reshape2)
+library(corrplot)
+library(PerformanceAnalytics)
+library(EnvStats)
+
+# library(gplot)
+
+shinyServer(function(input, output,session) {
+  
+Dataset <- reactive({
+  if (is.null(input$file)) { return(NULL) }
+  else{
+    Dataset <- as.data.frame(read.csv(input$file$datapath ,header=TRUE, sep = ","))
+    return(Dataset)
+  }
+})
+
+output$xvarselect <- renderUI({
+  if (identical(Dataset(), '') || identical(Dataset(),data.frame())) return(NULL)
+  
+  checkboxGroupInput("xAttr", "Select variables",
+                     colnames(Dataset()), colnames(Dataset()))
+  
+})
+
+Dataset.temp = reactive({
+  mydata = Dataset()[,c(input$xAttr)]
+})
+
+output$fxvarselect <- renderUI({
+  if (identical(Dataset(), '') || identical(Dataset(),data.frame())) return(NULL)
+  checkboxGroupInput("fxAttr", "Select factor (categorical) variables",
+                     colnames(Dataset.temp()) )
+  
+})
+
+mydata = reactive({
+  mydata = Dataset()[,c(input$xAttr)]
+
+  if (length(input$fxAttr) >= 1){
+  for (j in 1:length(input$fxAttr)){
+      mydata[,input$fxAttr[j]] = factor(mydata[,input$fxAttr[j]])
+  }
+  }
+  return(mydata)
+  
+})
+
+
+out = reactive({
+data = mydata()
+Missing=data[!complete.cases(data),]
+Dimensions = dim(data)
+Head = head(data)
+Tail = tail(data)
+Class = NULL
+for (i in 1:ncol(data)){
+  c1 = class(data[,i])
+  Class = c(Class, c1)
+}
+
+nu = which(Class %in% c("numeric","integer"))
+fa = which(Class %in% c("factor","character"))
+nu.data = data[,nu] 
+fa.data = data[,fa] 
+Summary = list(Numeric.data = round(stat.desc(nu.data)[c(4,5,6,8,9,12,13),] ,4), factor.data = describe(fa.data))
+# Summary = list(Numeric.data = round(stat.desc(nu.data)[c(4,5,6,8,9,12,13),] ,4), factor.data = describe(fa.data))
+
+a = seq(from = 0, to=200,by = 4)
+j = length(which(a < ncol(nu.data)))
+out = list(Dimensions = Dimensions,Summary =Summary ,Tail=Tail,fa.data,nu.data,a,j, Head=Head,MissingDataRows=Missing)
+return(out)
+})
+
+output$head = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    out()[8]
+  }
+})
+
+output$tail = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    out()[3]
+  }
+})
+
+output$missing = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    out()[9]
+  }
+})
+
+output$summary = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    out()[1:2]
+      }
+})
+
+# Select variables:
+output$outselect <- renderUI({
+  if (identical(Dataset(), '') || identical(Dataset(),data.frame())) return(NULL)
+  selectInput("yAttr", "Select variable for Rosner's outlier test",
+              colnames(Dataset.temp()), colnames(Dataset.temp())[1])
+  
+})
+
+output$outlier = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    rosnerTest(Dataset()[,input$yAttr])
+  }
+})
+
+
+output$heatmap = renderPlot({ 
+    qplot(x=Var1, y=Var2, data=melt(cor(out()[[5]], use = "pairwise.complete.obs")), fill=value, geom="tile") +
+    scale_fill_gradient2(limits=c(-1, 1))
+})
+
+plotsample =  reactive({
+  sample(1:nrow(mydata()), round( if (nrow(mydata()>200)) {200} else {nrow(mydata())}  ))
+})
+
+plot_data = reactive({
+  my_data = out()[[5]]
+  my_data[plotsample(),]
+})
+
+
+output$heatmap1 = renderPlot({ 
+  if (is.null(input$file)) {return(NULL)}
+  else {
+  chart.Correlation(plot_data(),hitogram=TRUE)
+  }
+})
+
+output$bplot = renderPlot({ 
+  if (is.null(input$file)) {return(NULL)}
+  else {
+    boxplot(out()[[5]])
+  }
+})
+
+output$correlation = renderPrint({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+  round(cor(out()[[5]], use = "pairwise.complete.obs"),2)
+  }
+  })
+
+output$corplot = renderPlot({
+  if (is.null(input$file)) {return(NULL)}
+  else {
+  my_data = out()[[5]]
+  cor.mat <- round(cor(my_data),2)
+  corrplot(cor.mat, 
+           type = "upper",    # upper triangular form
+           order = "hclust",  # ordered by hclust groups
+           tl.col = "black",  # text label color
+           tl.srt = 45) 
+  }
+  
+})
+
+
+output$downloadData <- downloadHandler(
+  filename = function() { "califhouse.csv" },
+  content = function(file) {
+    write.csv(read.csv("data/califhouse.csv"), file, row.names=F, col.names=F)
+  }
+)
+
+
+})
+
---
+++ Basic Statistics/ui.R
@@ -0,0 +1,60 @@
+####################################################
+#      Summary & BAsic Stats                        #
+####################################################
+
+library("shiny")
+#library("foreign")
+
+shinyUI(pageWithSidebar(
+  # Header:
+ # headerPanel("Regression App"),
+  headerPanel(title=div(img(src="isb.png",align = "right"), h2("Basic Stats App", style="bold"))),
+  
+  # Input in sidepanel:
+  sidebarPanel(
+
+    h5(p("Data Input")),
+    fileInput("file", "Upload input data (csv file with header)"),
+    h5(p("Data Selection")),
+    htmlOutput("xvarselect"),
+    htmlOutput("fxvarselect"),
+    htmlOutput("outselect"),
+    br()
+  ),
+  # Main:
+  mainPanel( 
+    
+    tabsetPanel(type = "tabs",
+                #
+                
+                tabPanel("Overview",
+                         h4(p("How to use this shiny application")),
+                         p("This shiny application require one data input from the user. To do so, click on the Browse (in left side-bar panel) and upload the csv data input file.
+                           Note that this application can read only csv file (comma delimited file), so if you don't have csv input data file, first convert your data in csv format 
+                           and then proceed. Make sure you have top row as variable names.",align="justify"),
+                         p("Once csv file is uploaded successfully, variables in the data file will reflect in left-side Data Selection Panel. Now you can select 
+                            dependent variable (Y Variable) from drop-down menu. By default all other remaining variables will be selected as explanatory variables (X variables). 
+                              If you want to drop any variable from explanatory variables, just uncheck that variable and it will be dropped from the model. 
+                            If any of the variables selected in explanatory variables is a factor variable, you can define that variable as factor variable just
+                            by selecting that variable in the last list of variables
+                           ",align="justify"),
+                         br(),
+                         h4(p("Download Sample Input Files")),
+                         # br(),
+                         downloadButton('downloadData', 'Download Sample Data (works only in browsers)'),
+                         br(),
+                         br(),
+                         p("*Please note that download will not work with RStudio interface. Download will work only in web-browsers."),
+                         ),
+                tabPanel("Data",h4("Data"), verbatimTextOutput("head"),verbatimTextOutput("tail"),
+                         h4("Data Summary"),verbatimTextOutput("summary")),
+                tabPanel("Missing Data", h4("Missing Data Rows"),verbatimTextOutput("missing")),
+                tabPanel("Correlation",h4("Correlation Table"), verbatimTextOutput("correlation"),
+                         h4("Correlation Visulization"),plotOutput("corplot")),
+                tabPanel("Box Plot", h4("Box Plots"),plotOutput("bplot")),
+                tabPanel("Data Visulization",h4("Discriptive Analytics (randomly selected 200 input data rows - If less than 200 rows in dataset, select whole dataset)"),plotOutput("heatmap1")),
+                tabPanel("Outliers", h4("selcet a variable in the left tab at the bottom, to test for Rosner's outliers in a selected varaible"),verbatimTextOutput("outlier"))
+                )
+      ) 
+    ) 
+  )
